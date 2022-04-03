62c69308c40277ba55ddb22c8b0eeadc9c503755
Victor <ivanvikvik@gmail.com>
2022-04-03 18:12:35 +0300

    Separate the implementation of the Student class from the interface part and put it in a separate file Student.cpp (отделяем реализацию класса Student от интерфейсной части и выносим в отдельный файл Student.cpp)

--- Student.h
+++ Student.h
@@ -7,47 +7,16 @@ using namespace std;
 class Student {
 private:
 	string name;
-	string age;
-
-	Student() {
-	}
+	string age;	
 
 public:
-	Student(string name, string age) {
-		this->name = name;
-		this->age = age;
-	}
-	
-	string getName() {
-		return name;
-	}
-
-	void setName(string initName) {
-		this->name = name;
-	}
-
-	int getAge() {
-		return stoi(age);
-	}
-
-	void setAge(int age) {
-		if (age >= 14 && age <= 80) {
-			this->age = to_string(age);
-		}
-	}
-
-	void setAge(string age) {
-		int number = stoi(age);
-		if (number >= 14 && number <= 80) {
-			this->age = age;
-		}
-	}
-
-	string getStringAge() {
-		return age;
-	}
-
-	string getInfo() {
-		return name + ": age = " + age;
-	}
+	Student();
+	Student(string name, string age);	
+	string getName();
+	void setName(string initName);
+	int getAge();
+	void setAge(int age);
+	void setAge(string age);
+	string getStringAge();
+	string getInfo();
 };
